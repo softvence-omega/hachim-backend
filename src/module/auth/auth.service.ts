a299e9f3b97516b278b8ac87b20a0e52ee2b9da5
MD.SHIPON <shipon@bdcallings-Mac-mini-5.local>
2025-06-12 06:46:18 +0600

    update user schema in schema.prisma file

---
+++ src/module/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from "bcrypt"
+
+
+@Injectable()
+export class AuthService {
+
+constructor(private prisma:PrismaService){}
+
+hashData(data:string){
+    return bcrypt.hash(data,process.env.SOLT_ROUND as string)
+}
+
+}
