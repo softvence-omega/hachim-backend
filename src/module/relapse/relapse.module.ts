2cddf5bbf767416e2432b6231232ce0d0a14b492
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-06-14 01:43:05 +0600

    relapse refactor

--- src/module/relapse/relapse.module.ts
+++ src/module/relapse/relapse.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { RelapseController } from './relapse.controller';
 import { RelapseService } from './relapse.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { MoodTrackService } from '../mood-track/mood-track.service';
 
 @Module({
   controllers: [RelapseController],
   providers: [RelapseService, PrismaService],
+  
 })
 export class RelapseModule {}
--- src/module/relapse/relapse.service.ts
+++ src/module/relapse/relapse.service.ts
@@ -1,45 +1,77 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateRelapseDto } from './dto/relapse.create.dto';
-import { UpdateRelapseDto } from './dto/relapse.update.dto';
 
 
 @Injectable()
 export class RelapseService {
   constructor(private prisma: PrismaService) {}
 
-  async createRelapse(dto: CreateRelapseDto) {
-    const relapse = await this.prisma.relapse.create({
-      data: {
+  async createOrUpdateRelapse(dto: CreateRelapseDto,userId:string) {
+    const relapse = await this.prisma.relapse.upsert({
+      where: {
+        userId
+      },
+      update: {
+        ...dto,
+        updatedAt: new Date(),
+      },
+      create: {
         ...dto,
         isDeleted: false,
+        userId
       },
     });
     return relapse;
   }
 
-  async updateRelapse(dto: UpdateRelapseDto) {
-    const relapse = await this.prisma.relapse.findUnique({
-      where: { id: dto.id },
+  async resetOptionalFields(userId: string) {
+    
+    const existing = await this.prisma.relapse.findUnique({
+      where: { userId },
     });
 
-    if (!relapse) {
-      throw new NotFoundException('Relapse entry not found');
+    if (!existing) {
+      throw new NotFoundException('Relapse record not found for this user');
     }
 
     const updated = await this.prisma.relapse.update({
-      where: { id: dto.id },
+      where: { userId },
       data: {
-        ...dto,
-        updatedAt: new Date(),
+        Mood: null,
+        urg: null,
+        Triggers: null,
+        note: null,
+        spendDate: null,
+        level: null,
+       
       },
     });
 
-    // If marked as deleted, delete related mood/sleep/motion tracks
-    if (dto.isDeleted) {
-      
-    }
+
+await Promise.all([
+  this.prisma.moodTrack.upsert({
+    where: { userId },
+    update: { value: 0 },
+    create: { value: 0, userId },
+  }),
+
+  this.prisma.motivationTrack.upsert({
+    where: { userId },
+    update: { motivation: "" },
+    create: { motivation: "", userId },
+  }),
+
+  this.prisma.sleepTrack.upsert({
+    where: { userId },
+    update: { hours: 0 },
+    create: { hours: 0, userId },
+  }),
+]);
+
+
 
     return updated;
   }
+
 }
